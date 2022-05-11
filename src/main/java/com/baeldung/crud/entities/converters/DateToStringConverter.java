d9748429b3b95d01c56818d909a6efa7ca6f9c22
Albert-Dzh <tryhardgopru@gmail.com>
2022-05-11 17:40:26 +0300

    impl converter for UserItem dt field

---
+++ src/main/java/com/baeldung/crud/entities/converters/DateToStringConverter.java
@@ -0,0 +1,20 @@
+package com.baeldung.crud.entities.converters;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+@Converter
+public class DateToStringConverter implements AttributeConverter<LocalDate, String> {
+    @Override
+    public String convertToDatabaseColumn(LocalDate date) {
+        return date == null ? "" : String.valueOf(date.atStartOfDay(ZoneId.systemDefault()).toEpochSecond());
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(String time) {
+        return time.isEmpty() ? null : Instant.ofEpochSecond(Integer.parseInt(time)).atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+}
