041101df80b5fcc2eb4e849edc0200670ab26637
Albert-Dzh <tryhardgopru@gmail.com>
2022-05-12 11:09:27 +0300

    drop useless annotation

--- src/main/java/com/baeldung/crud/repositories/UserRepository.java
+++ src/main/java/com/baeldung/crud/repositories/UserRepository.java
@@ -6,9 +6,7 @@ import com.baeldung.crud.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     Page<User> findAllByLogin(String login, Pageable pageable);
